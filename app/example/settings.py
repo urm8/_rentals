89b7666059cf278bcf339cbd1eb62f3faf1cebc7
urm8 <arctgisnil@gmail.com>
2022-04-13 22:04:10 +0300

    add nginx

--- app/example/settings.py
+++ app/example/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-#rfeua=pcv4j8@3%-61og9xv2_2w^q^4#=lw6kqmtbl2y-tes0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'true') == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split()
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['http://localhost']
 
 LOCAL_APPS = [
     'rentals',
@@ -117,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
